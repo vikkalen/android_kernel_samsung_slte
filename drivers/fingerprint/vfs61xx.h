acf810fa76f0549a6bedffed5873af52482ffeae
Martin Bouchet <tincho5588@gmail.com>
2017-08-14 22:13:05 -0300

    Disable FEATURE_SPI_WAKELOCK, as on klte
    That wakelock is hold forever, thus disallowing deep sleep.  As on klte,
    it's completely redundant and can just go away.
    
    Change-Id: If2ed7c37f554d25912f19710d70ead3614224feb

--- drivers/fingerprint/vfs61xx.h
+++ drivers/fingerprint/vfs61xx.h
@@ -69,13 +69,6 @@
 /* Timeout value for polling DRDY signal assertion */
 #define DRDY_TIMEOUT_MS      40
 
-#ifdef CONFIG_SEC_FACTORY
-#undef ENABLE_SENSORS_FPRINT_SECURE
-#else
-#define ENABLE_SENSORS_FPRINT_SECURE
-#define FEATURE_SPI_WAKELOCK
-#endif /* CONFIG_SEC_FACTORY */
-
 /*
  * Definitions of structures which are used by IOCTL commands
  */
